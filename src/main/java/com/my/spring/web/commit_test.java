bdad7652aae681b0ab7adf77ba1c55f17ed4b133
DESKTOP-FLT66P5\Credif <mskim@credif.co.kr>
2022-12-27 11:20:21 +0900

    commit test

---
+++ src/main/java/com/my/spring/web/commit_test.java
@@ -0,0 +1,11 @@
+package com.my.spring.web;
+
+public class commit_test {
+	public void print() {
+		int test = 1;
+		String str = "hello";
+		
+		System.out.println(str + "world");
+		System.out.println(test);
+	}
+}
